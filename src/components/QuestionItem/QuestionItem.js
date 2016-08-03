361f4bbb88866aba2a58b7a42539924f6839db99
Andrew M <me@r3nya.ru>
2016-08-03 23:22:39 +0300

    Deleted stuff

---
+++ src/components/QuestionItem/QuestionItem.js
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class QuestionItem extends Component {
+  static propTypes = {
+
+  };
+
+  render() {
+    return (
+      <div>MyComponent</div>
+    );
+  }
+}
