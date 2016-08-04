289fd5751aa8f8cf6226202947cf9bc0a6ba030d
Andrew M <me@r3nya.ru>
2016-08-05 00:50:13 +0300

    Added skelethon for CustomFields components

---
+++ src/components/fields/index.js
@@ -0,0 +1,6 @@
+export * from './Checkboxes';
+export * from './FileUpload';
+export * from './ParagraphText';
+export * from './RadioButtons';
+export * from './Select';
+export * from './SingleLineText';
