2f49e2d4410a51a2b51f2f1ea502fcd9aa78d38c
Andrey Makarov <me@r3nya.ru>
2016-08-10 18:13:49 +0300

    Added lostgrid

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,7 @@ const common = {
   postcss: [
     autoprefixer({ browsers: ['last 2 versions'] }),
     require('postcss-nested'),
+    require('lost'),
     require('postcss-import'),
     require('postcss-css-variables'),
     require('postcss-apply'),
