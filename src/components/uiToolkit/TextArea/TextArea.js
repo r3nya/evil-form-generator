331377a8ebfa646e54a13544387d6275f243ea1b
Andrey Makarov <me@r3nya.ru>
2016-08-10 11:01:37 +0300

    Added TextArea component

--- src/components/uiToolkit/TextArea/TextArea.js
+++ src/components/uiToolkit/TextArea/TextArea.js
@@ -1,12 +1,31 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes } from 'react'
+// import cx from 'classnames'
+import camelcase from 'camelcase'
+import styles from './TextArea.css'
 
 export const TextArea = props => {
+  const { label, ...rest } = props
+  const idAttr = label ? camelcase(label) : ''
 
   return (
-    <div>TextArea</div>
-  );
+    <div className={styles.cont}>
+      {label &&
+        <label
+          htmlFor={idAttr}
+          className={styles.label}
+        >
+          {label}
+        </label>
+      }
+      <textarea
+        id={idAttr}
+        className={styles.txtArea}
+        {...rest}
+      ></textarea>
+    </div>
+  )
 }
 
 TextArea.propTypes = {
-
+  label: PropTypes.string
 }
