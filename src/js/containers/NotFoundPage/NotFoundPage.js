39c944f001b353eaae88e1d9bf7acd28ca5dd9f3
Andrey Makarov <me@r3nya.ru>
2016-08-11 19:12:00 +0300

    Added awesome 404 page

--- src/js/containers/NotFoundPage/NotFoundPage.js
+++ src/js/containers/NotFoundPage/NotFoundPage.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import styles from './NotFoundPage.css'
+import pic from './mars_attacks.jpg'
 
 export const NotFoundPage = () => (
-  <div className={styles.oops}>404! 😼</div>
+  <div className={styles.oops}>
+    <div>404!</div>
+    <img src={pic} alt="Mars Attacks"/>
+  </div>
 )
