c6458c203ae4be9b9111b2cb8a7de38d1e727489
Andrey Makarov <me@r3nya.ru>
2016-08-18 15:15:58 +0300

    Updated styles

--- src/js/components/ListItem/ListItem.js
+++ src/js/components/ListItem/ListItem.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
 import { Button, DeleteIcon, EditIcon } from 'components/uiToolkit'
+import cx from 'classnames'
 import styles from './ListItem.css'
 
 export const ListItem = props => {
@@ -8,15 +9,22 @@ export const ListItem = props => {
 
   return (
     <li className={styles.item}>
-      <Link to={`/view/${id}`}>
+      <Link
+        className={cx('cell cell__8of12 cell__sm__10of12', styles.link)}
+        to={`/view/${id}`}
+      >
         {id} – {description} ({createdAt})
       </Link>
       <Button
+        button
+        className="cell"
+        style="outline"
         to={`/edit/${id}`}
       >
         <EditIcon />
       </Button>
       <Button
+        className="cell"
         style="outline"
         onClick={() => onDeleteForm(id)}
       >
--- src/js/components/uiToolkit/Button/Button.js
+++ src/js/components/uiToolkit/Button/Button.js
@@ -4,12 +4,13 @@ import cx from 'classnames'
 import styles from './Button.css'
 
 export const Button = props => {
-  const { children, className, size, disabled, to, style, ...rest } = props
+  const { children, className, size, button, disabled, to, style, ...rest } = props
   const Element = to ? Link : 'button'
   const aria = to ? 'link' : 'button'
   const classes = cx(styles.button, className, {
     [styles.outline]: style === 'outline',
     [styles.primary]: style === 'primary',
+    [styles.button]: button,
     [styles.transparent]: style === 'transparent',
     [styles.large]: size === 'large',
     [styles.small]: size === 'small',
@@ -33,6 +34,7 @@ Button.propTypes = {
   className: PropTypes.string,
   active: PropTypes.bool,
   size: PropTypes.oneOf(['large', 'small']),
+  button: PropTypes.bool,
   disabled: PropTypes.bool,
   to: PropTypes.string,
   style: PropTypes.oneOf(['outline', 'primary', 'transparent'])
