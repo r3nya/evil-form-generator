687938c3f5af97d53cf24cf6d4dd17b318d51ed3
Andrew M <me@r3nya.ru>
2016-09-24 23:39:57 +0300

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,22 @@
 
 set -o errexit -o nounset
 
-rev=$(git rev-parse --short HEAD)
+GH_REPO="@github.com/r3nya/evil-form-generator.git"
+FULL_REPO="https://$GH_TOKEN$GH_REPO"
+REV=$(git rev-parse --short HEAD)
+OUT=$TRAVIS_BUILD_DIR/out
 
-cd $TRAVIS_BUILD_DIR/dist
+mkdir -p $OUT/static
+cp $TRAVIS_BUILD_DIR/index.html $OUT
+cp $TRAVIS_BUILD_DIR/dist/* $OUT/static/
+
+cd $OUT
 
 git init
 git config user.name "Andrew M."
 git config user.email "me@r3nya.ru"
 
-git remote add upstream "https://$GH_TOKEN@github.com/r3nya/evil-form-generator.git"
-git fetch upstream
-git reset upstream/gh-pages
-
 git add -A .
-git commit -m "rebuild pages at ${rev}"
+git commit -m "rebuild pages at ${REV}"
 git push -q upstream HEAD:gh-pages
+git push -q $FULL_REPO master:gh-pages
